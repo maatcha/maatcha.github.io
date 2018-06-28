5fa53d7457fcf71752b23d56886b86f3f5dfbf55
matcha <Mattcha54@gmail.com>
2018-06-28 15:57:54 +0200

    toujours pas de violet

--- BetterMotherFuckingWebsite/main.js
+++ BetterMotherFuckingWebsite/main.js
@@ -1,6 +1,6 @@
 alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
 
-function purpleBack(button) {
+function purpleBack(getElementById("button")) {
 	var body = document.getElementById("body");
 	body.style.backgroundColor = "violet";
 }
